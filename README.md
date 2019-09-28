086f5af0ade1c112dcfc53c53049a92d55f666b9
JasonZhangblue <1172279548@qq.com>
2019-09-28 12:48:25 +0800

    DTW的实现

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ProjectLog
 
 ## 项目日志
-
+[语音识别的一些基础知识](https://blog.csdn.net/qq_34231800/article/details/80189617)
 本项目主要内容在于孤立词的评分以及其发音反馈，其中功能将逐步实现
 
 由于需要部署到小程序，因此有域名www.kaldi-speech.cn
@@ -19,10 +19,13 @@
    [MFCC简单教程](http://www.practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/)  
    [python_speech_features官方使用文档](https://python-speech-features.readthedocs.io/en/latest/)   
    [使用Python_speech_features进行MFCC提取](https://www.jianshu.com/p/e32d2d5ccb0d)    
-   [MFCC的提取过程讲解](https://blog.csdn.net/zouxy09/article/details/9156785)   
+   [MFCC的提取过程讲解](https://blog.csdn.net/zouxy09/article/details/9156785)      
+   [MFCC各种教程](https://blog.csdn.net/zzc15806/article/details/79246716)
    [动态时间规整](https://www.cnblogs.com/zhizhan/p/4419066.html)    
-   [MFCC和DTW的实现](https://blog.csdn.net/qq_31821675/article/details/80195315)  
-   换行
+   [DTW](https://cnblogs.com/zhizhan/p/4419066.html)
+   [MFCC和DTW的实现](https://blog.csdn.net/qq_31821675/article/details/80195315 )  
+
+   结果说明：无法定量的描述相对的评分，最终得到的是两者特征向量的平均匹配值，无法达到预想的使用标准，可以直接放弃
 2. #### [基于HMM+DNN的方法]()
    [HMM的简单说明](http://www.practicalcryptography.com/miscellaneous/machine-learning/hidden-markov-model-hmm-tutorial/)
 
--- 基于DTW与标准语音对比/speech_features.py
+++ 基于DTW与标准语音对比/speech_features.py
@@ -1,21 +1,85 @@
 from python_speech_features import mfcc
 from python_speech_features import delta
-import numpy
+import numpy as np
 from python_speech_features import logfbank
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
+from numpy import array, zeros, full, argmin, inf, ndim
+from scipy.spatial.distance import cdist
+from math import isinf,sqrt
+import dtw
 
-(rate, sig) = wav.read("F://University//国创//ProjectLog//基于DTW与标准语音对比//00f0204f_nohash_1.wav")
 
-mfcc_feat = mfcc(sig, rate, winfunc=numpy.hamming)
-plt.plot(sig)
-plt.show()
-plt.plot(mfcc_feat[1])
-plt.show()
-d_mfcc_feat = delta(mfcc_feat, 2)
-a=numpy.array(d_mfcc_feat)
-print(a)
-plt.plot(d_mfcc_feat)
-plt.show() 
-fbank_feat = logfbank(sig,rate)
-print(mfcc_feat.shape)
+def dtw(x, y, warp=1, w=inf, s=1.0):
+    #使用其他的代码对dtw进行了初步的实现，下一步是自己写该算法的代码
+    assert len(x)
+    assert len(y)
+    assert isinf(w) or (w >= abs(len(x) - len(y)))
+    assert s > 0
+
+    
+    r, c = len(x), len(y)
+    if not isinf(w):
+        D0 = full((r + 1, c + 1), inf)
+        for i in range(1, r + 1):
+            D0[i, max(1, i - w):min(c + 1, i + w + 1)] = 0
+        D0[0, 0] = 0
+    else:
+        D0 = zeros((r + 1, c + 1))
+        D0[0, 1:] = inf
+        D0[1:, 0] = inf
+    D1 = D0[1:, 1:]  # view
+    for i in range(r):
+        for j in range(c):
+            if (isinf(w) or (max(0, i - w) <= j <= min(c, i + w))):
+                D1[i, j] = np.linalg.norm(x[i]-y[i])
+    C = D1.copy()
+    jrange = range(c)
+    for i in range(r):
+        if not isinf(w):
+            jrange = range(max(0, i - w), min(c, i + w + 1))
+        for j in jrange:
+            min_list = [D0[i, j]]
+            for k in range(1, warp + 1):
+                i_k = min(i + k, r)
+                j_k = min(j + k, c)
+                min_list += [D0[i_k, j] * s, D0[i, j_k] * s]
+            D1[i, j] += min(min_list)
+    if len(x) == 1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    return D1[-1, -1] / sum(D1.shape),D1
+def dtw_test(mfcc_standard,mfcc_test):
+    #使用动态时间规整对两者的mfcc特征进行一个比较，并通过评分公式给出评分
+    M,N=len(mfcc_standard),len(mfcc_test)
+    print(M,N)
+    cost=np.zeros((M,N))
+    for m in range(0,M):
+        for n in range(0,N):
+            d[m,n]=np.linalg.norm(mfcc_standard[m],mfcc_test[n])
+
+def distance(x,y):
+    return sqrt(x*x+y*y)
+if __name__==  "__main__":
+
+    (rate, sig) = wav.read(
+        "F://University//国创//ProjectLog//基于DTW与标准语音对比//00f0204f_nohash_1.wav")
+    (rate_test, sig_test) = wav.read(
+        "F://University//国创//ProjectLog//基于DTW与标准语音对比//0c2d2ffa_nohash_0.wav")
+    """ plt.plot(sig_test) """
+    mfcc_feat = mfcc(sig, rate, winfunc=np.hamming)
+    mfcc_feat_test = mfcc(sig_test, rate_test, winfunc=np.hamming)
+    d_mfcc_feat = delta(mfcc_feat, 2)
+    d_mfcc_feat_test = delta(mfcc_feat_test, 2)
+    fbank_feat = logfbank(sig_test, rate_test)
+    plt.plot(d_mfcc_feat)
+    a=np.array(mfcc_feat)
+    print(a)
+    print(np.shape(mfcc_feat))
+    #通过DTW比较两个语音的相似程度
+    a,D1=dtw(mfcc_feat, mfcc_feat_test,
+                 warp=1,w=inf,  s=1.0)
+    print(a)
+    plt.plot(D1)
+    plt.show()
